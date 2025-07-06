fd83ba871854577cdbd9c0ad42d97acafc433cd5
Sam Collier <samcollier916@gmail.com>
2025-07-06 17:52:34 +0100

    no imgui.ini

--- src/client/Window/private/RaylibWindow.cpp
+++ src/client/Window/private/RaylibWindow.cpp
@@ -8,16 +8,19 @@ namespace goop::Window
 {
 void CreateWindow( int width, int height, const char* title )
 {
-	SetTraceLogLevel(LOG_ERROR);
+	SetTraceLogLevel( LOG_ERROR );
 	InitWindow( width, height, title );
-	SetWindowState(FLAG_WINDOW_RESIZABLE);
+	SetWindowState( FLAG_WINDOW_RESIZABLE );
 	rlImGuiSetup( true );
+	auto& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
+	io.LogFilename = nullptr;
 }
 void Draw()
 {
 	SetExitKey( KEY_NULL );
 	BeginDrawing();
-	ClearBackground(BLACK);
+	ClearBackground( BLACK );
 	rlImGuiBegin();
 	ImGui::ShowDemoWindow();
 	rlImGuiEnd();
